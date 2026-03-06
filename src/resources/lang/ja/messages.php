b1b7d6bb4314ac52f6e7772c7f136dc99d0eeeb7
k-masudaIC <k.masuda@c-innocent.com>
2026-03-06 13:00:44 +0900

    日本語に変更

---
+++ src/resources/lang/ja/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Dashboard' => 'ダッシュボード',
+    'Profile' => 'プロフィール',
+    'Log Out' => 'ログアウト',
+    'Email' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember me' => 'ログイン状態を保持する',
+    'Forgot your password?' => 'パスワードをお忘れですか？',
+    'Log in' => 'ログイン',
+    'Name' => '名前',
+    'Confirm Password' => 'パスワード（確認）',
+    'Already registered?' => 'すでに登録済みですか？',
+    'Register' => '新規登録',
+];
